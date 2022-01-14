4cb05dee8aef51d536200c76748e66855f64c4cd
Carlos <csantosdeveloper@gmail.com>
2022-01-14 22:15:47 +0100

    Create api-gateway-service for routing among other things

---
+++ api-gateway-service/src/main/java/com/adidas/apigatewayservice/ApiGatewayServiceApplication.java
@@ -0,0 +1,17 @@
+package com.adidas.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+
+}
