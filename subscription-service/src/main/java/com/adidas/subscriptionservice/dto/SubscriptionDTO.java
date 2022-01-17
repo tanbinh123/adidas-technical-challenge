8283a481fff79150a4a64910ff1aa24ede95ad42
Carlos <csantosdeveloper@gmail.com>
2022-01-17 19:13:58 +0100

    Add ID property to send it to email-service

--- subscription-service/src/main/java/com/adidas/subscriptionservice/dto/SubscriptionDTO.java
+++ subscription-service/src/main/java/com/adidas/subscriptionservice/dto/SubscriptionDTO.java
@@ -11,6 +11,7 @@ import java.util.Date;
 @NoArgsConstructor
 public class SubscriptionDTO {
 
+    private Integer id;
     private String email;
     private String name;
     private String gender;
