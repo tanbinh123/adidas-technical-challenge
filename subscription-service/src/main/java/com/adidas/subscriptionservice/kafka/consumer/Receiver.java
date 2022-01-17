4dac466bdaac2e8540ad74f6516f716bc91a35ae
Carlos <csantosdeveloper@gmail.com>
2022-01-17 08:36:41 +0100

    Add Kafka receiver functionalities

---
+++ subscription-service/src/main/java/com/adidas/subscriptionservice/kafka/consumer/Receiver.java
@@ -0,0 +1,28 @@
+package com.adidas.subscriptionservice.kafka.consumer;
+
+import com.adidas.subscriptionservice.dto.SubscriptionDTO;
+import com.adidas.subscriptionservice.service.ISubscriptionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Receiver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    @Autowired
+    private ISubscriptionService subscriptionService;
+
+    @KafkaListener(topics = "${spring.kafka.topic.subscribe}")
+    public void receive(SubscriptionDTO subscriptionDTO) {
+        LOGGER.info("received subscriptionDTO='{}'", subscriptionDTO);
+        subscriptionService.createSubscription(subscriptionDTO);
+        latch.countDown();
+    }
+
+}
