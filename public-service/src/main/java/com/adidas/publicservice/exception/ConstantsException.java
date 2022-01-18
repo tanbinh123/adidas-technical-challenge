214a434ff04a901a0c527dcdd6951ebaccf4152a
Carlos <csantosdeveloper@gmail.com>
2022-01-18 21:34:19 +0100

    Add constants and exception handler

---
+++ public-service/src/main/java/com/adidas/publicservice/exception/ConstantsException.java
@@ -0,0 +1,8 @@
+package com.adidas.publicservice.exception;
+
+public class ConstantsException {
+
+    public static final String BAD_REQUEST_MESSAGE = "Bad Request message";
+    public static final String INTERNAL_ERROR_MESSAGE = "Internal Error message";
+
+}
---
+++ public-service/src/main/java/com/adidas/publicservice/util/ConstantsUtil.java
@@ -0,0 +1,9 @@
+package com.adidas.publicservice.util;
+
+public class ConstantsUtil {
+
+    public static final String topicSent = "The controller has sent the Kafka Topic successfully";
+    public static final String senderError = "The controller couldn't sent the Kafka Topic";
+    public static final String OK_MESSAGE = "Ok message";
+
+}
